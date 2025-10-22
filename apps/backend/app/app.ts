117723ab2e0a053b7fba096b252f72c974caf643
Dion Short <dion.s@disco.ac>
2025-10-23 00:26:39 +0100

    connect to database

--- apps/backend/app/app.ts
+++ apps/backend/app/app.ts
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 import path from 'path';
 
 import log from '../logging/log';
@@ -15,6 +16,8 @@ log.info(`inDevelopmentMode: ${inDevelopmentMode}`);
 
 const app = express();
 app.disable('x-powered-by');
+
+mongoose.connect('process.env.MONGO_URL');
 
 app.use(
   express.json({
