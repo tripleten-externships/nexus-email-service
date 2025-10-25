d25415fe9c400f0becb97ffe61e55ba7f76df7b3
Dion Short <dion.s@disco.ac>
2025-10-25 19:43:44 +0100

    create user schema and model, and connect mongoose

---
+++ apps/backend/app/models/user.ts
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from 'mongoose';
+
+const userSchema: Schema = new Schema({
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true, minlength: 2, maxlength: 30 },
+  role: { type: String, required: true, enum: ['admin', 'editor', 'viewer'] },
+  password: { type: String, required: true, minlength: 8 },
+  loginTracking: {
+    lastLogin: { type: Date, default: null },
+    loginCount: { type: Number, default: 0 },
+  },
+  preferences: { type: Map, of: String },
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
