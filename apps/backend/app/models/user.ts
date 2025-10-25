f7b07977fff72c47e8f14b2040b58e468790df1f
Dion Short <dion.s@disco.ac>
2025-10-25 21:31:07 +0100

    install validator and add validation to user schema

--- apps/backend/app/models/user.ts
+++ apps/backend/app/models/user.ts
@@ -1,7 +1,17 @@
 import mongoose, { Schema } from 'mongoose';
+import validator from 'validator';
 
 const userSchema: Schema = new Schema({
-  email: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+    },
+  },
   name: { type: String, required: true, unique: true, minlength: 2, maxlength: 30 },
   role: { type: String, required: true, enum: ['admin', 'editor', 'viewer'] },
   password: { type: String, required: true, minlength: 8 },
