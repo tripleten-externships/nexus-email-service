be15263f628f041803e0fb5be0cd0a8fe88e4cfa
P.R. Neider-Ball <phillrballn@icloud.com>
2025-10-31 12:00:12 -0500

    added method interface

--- packages/types/src/recipient.ts
+++ packages/types/src/recipient.ts
@@ -1,4 +1,4 @@
-import { Types } from 'mongoose';
+import mongoose, { Types } from 'mongoose';
 
 export interface IRecipient {
   _id: Types.ObjectId;
@@ -12,10 +12,10 @@ export interface IRecipient {
       campaignId: Types.ObjectId;
       openedEmails: number;
       linksClicked: number;
-      deliveredEmails?: number;
-      bouncedEmails?: number;
-      unsubscribedAt?: Date;
-      spamReports?: number;
+      deliveredEmails: number;
+      bouncedEmails: number;
+      unsubscribedAt: Date;
+      spamReports: number;
       lastEngagedAt: Date;
     },
   ];
@@ -32,4 +32,16 @@ export interface IRecipient {
   customAttributes: Record<string, string | number | boolean | null>;
   createdAt: Date;
   updatedAt: Date;
+}
+
+export interface IRecipientMethods {
+  updateStatus(newStatus: IRecipient['status']): Promise<IRecipient>;
+  ensureListsArray(): void;
+  addToList(listId: Types.ObjectId): Promise<IRecipient>;
+  removeFromList(listId: Types.ObjectId): Promise<IRecipient>;
+  updateAggregateEngagement(): Promise<IRecipient>;
+  recordEngagement(
+    campaignId: Types.ObjectId,
+    field: keyof IRecipient['engagementHistory'][0]
+  ): Promise<IRecipient>;
 }
