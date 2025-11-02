bbd9466ddba5c143113719fec44c61d89692b731
Dion Short <dion.s@disco.ac>
2025-11-02 09:32:22 +0000

    add authorisation middleware

---
+++ apps/backend/app/middleware/auth.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import * as jwt from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+// Create a custom Request interface to add the 'user' property
+interface CustomRequest extends Request {
+  user?: string | jwt.JwtPayload;
+}
+
+export const auth = (req: CustomRequest, res: Response) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res.status(401).send({ message: 'Authorization required.' });
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err: unknown) {
+    return res.status(401).send({ message: 'Authorization required.' });
+  }
+
+  return (req.user = payload);
+};
