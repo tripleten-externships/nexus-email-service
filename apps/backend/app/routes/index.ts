568bd7eaffbfd3eec0a6db49527ae2d851a14372
Dion Short <dion.s@disco.ac>
2025-11-03 18:03:12 +0000

    update user route name in index.ts

--- apps/backend/app/routes/index.ts
+++ apps/backend/app/routes/index.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import userModel from './users';
+import userRouter from './users';
 import { login } from '../controllers/users';
 
 const router = Router();
 
-router.use('/users', userModel);
+router.use('/users', userRouter);
 router.post('/signin', login);
 
 export default router;
