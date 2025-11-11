025eb6b0569d3ac3c04c9b26b3f6645716535898
costjonah <jonah.cost@tripleten.com>
2025-11-11 12:45:42 -0500

    [NES-18] update file name, add validator types

--- apps/backend/app/models/index.ts
+++ apps/backend/app/models/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import log from '../../logging/log';
 
 // import './userModel';
-import './emailEvent.model';
+import './emailEvent';
 
 /**
  * Initialize all models and ensure their collections exist in MongoDB
