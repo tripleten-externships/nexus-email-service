f3eda41f8e856c1fa7f1f01d7e8ced0ac22062a0
P.R. Neider-Ball <phillrballn@icloud.com>
2025-11-29 10:26:48 -0600

    feat: add inAppMessage interface

---
+++ packages/types/src/inAppMessage.ts
@@ -0,0 +1,18 @@
+import { Types } from 'mongoose';
+
+export type InAppMessageType = 'user_message' | 'system_notification';
+
+export interface IInAppMessage {
+  _id: Types.ObjectId;
+  messageType: InAppMessageType;
+  senderId: Types.ObjectId;
+  recipientId: Types.ObjectId;
+  subject: String;
+  body: String;
+  isRead: boolean;
+  readAt: Date | null;
+  deletedBySender: boolean;
+  deletedByRecipient: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
